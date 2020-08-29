fa9104fb5482368ab8d2d0d53c940b2aeae77296
Luis Tuason <philipluistuason@gmail.com>
2020-08-29 23:30:28 +0800

    Workshop #2: Front-End

--- ISworkshop/urls.py
+++ ISworkshop/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
+    path('form/', views.form, name='form')
 ]
--- ISworkshop/views.py
+++ ISworkshop/views.py
@@ -5,3 +5,13 @@ def index(request):
   context = { }
 
   return render(request, 'ISworkshop/index.html', context=context)
+
+def profile(request):
+  context = { }
+
+  return render(request, 'ISworkshop/profile.html', context=context)
+
+def form(request):
+  context = { }
+
+  return render(request, 'ISworkshop/form.html', context=context)
